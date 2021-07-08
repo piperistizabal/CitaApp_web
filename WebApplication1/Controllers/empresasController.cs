ed0bf05caf190382782ac58ee0f3d7e94b263b5a
Jhoan1309 <jstivenlaiton@gmail.com>
2021-07-08 14:35:24 -0500

    Tercera Version

---
+++ WebApplication1/Controllers/empresasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class empresasController : Controller
+    {
+        private CitAppEntities db = new CitAppEntities();
+
+        // GET: empresas
+        public ActionResult Index()
+        {
+            return View(db.empresas.ToList());
+        }
+
+        // GET: empresas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            empresas empresas = db.empresas.Find(id);
+            if (empresas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresas);
+        }
+
+        // GET: empresas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: empresas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,identificacion,nombre,razonSocial,tipo,direccion,telefono,logo,correoElectronico,estado")] empresas empresas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.empresas.Add(empresas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(empresas);
+        }
+
+        // GET: empresas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            empresas empresas = db.empresas.Find(id);
+            if (empresas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresas);
+        }
+
+        // POST: empresas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,identificacion,nombre,razonSocial,tipo,direccion,telefono,logo,correoElectronico,estado")] empresas empresas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empresas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(empresas);
+        }
+
+        // GET: empresas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            empresas empresas = db.empresas.Find(id);
+            if (empresas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresas);
+        }
+
+        // POST: empresas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            empresas empresas = db.empresas.Find(id);
+            db.empresas.Remove(empresas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
